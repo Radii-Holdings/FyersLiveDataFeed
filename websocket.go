0990a86e0827301e4beb0766a264072601d01bec
Aloy Aditya Sen <aloysen951@gmail.com>
2022-10-24 12:20:12 +0530

    adding the filesystem changes to thse system

--- websocket.go
+++ websocket.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -18,6 +19,7 @@ import (
 
 const (
 	YYYYMMDD = "2006-01-02"
+	HHMMSS   = "17:06:04 PM"
 )
 
 func JsonToBson(message []byte) ([]byte, error) {
@@ -42,53 +44,83 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	now := time.Now().UTC()
+	now := time.Now()
+	csvfile, err := os.Create("./dataset/raw/" + now.Format(YYYYMMDD) + ".csv")
+	if err != nil {
+		panic(err)
+	} else {
+		csvwriter := csv.NewWriter(csvfile)
 
-	onConnectFunc := func() {
-		fmt.Println("watch subscription is connected")
-	}
+		onConnectFunc := func() {
+			fmt.Println("watch subscription is connected")
+			row := []string{"Mongo Object Id", "Database Name", "collection name", "Upsert time"}
+			_ = csvwriter.Write(row)
+			csvwriter.Flush()
 
-	onMessageFunc := func(notification api.Notification) {
-		// fmt.Println(notification.Type, notification.SymbolData)
-		// fmt.Println(notification.SymbolData.LowPrice, notification.SymbolData.Symbol)
-
-		DataPack, err := json.Marshal(notification)
-		datumNode, err := JsonToBson([]byte(DataPack))
-		if err != nil {
-			fmt.Printf("Error: %s", err)
-			return
-		}
-		fmt.Println(datumNode)
-		// var doc interface{}
-		baseString1 := strings.ReplaceAll(notification.SymbolData.Symbol, ":", "-")
-		baseString2 := strings.ReplaceAll(baseString1, ".", "")
-		TickerCollection := client.Database(baseString2).Collection(now.Format(YYYYMMDD))
-		result, err := TickerCollection.InsertOne(context.TODO(), datumNode)
-		if err != nil {
-			panic(err)
-		} else {
-			fmt.Println("json inserted ", result)
 		}
 
+		onMessageFunc := func(notification api.Notification) {
+			// fmt.Println(notification.Type, notification.SymbolData)
+			// fmt.Println(notification.SymbolData.LowPrice, notification.SymbolData.Symbol)
+
+			DataPack, err := json.Marshal(notification)
+			datumNode, err := JsonToBson([]byte(DataPack))
+			if err != nil {
+				fmt.Printf("Error: %s", err)
+				return
+			}
+			// fmt.Println(datumNode)
+			baseString1 := strings.ReplaceAll(notification.SymbolData.Symbol, ":", "-")
+			baseString2 := strings.ReplaceAll(baseString1, ".", "")
+
+			TickerCollection := client.Database(baseString2).Collection(now.Format(YYYYMMDD))
+			result, err := TickerCollection.InsertOne(context.TODO(), datumNode)
+			if err != nil {
+				panic(err)
+			} else {
+				fmt.Println("json inserted ", result.InsertedID)
+				baseResult := fmt.Sprint(result.InsertedID)
+				inter1 := strings.Trim(baseResult, "ObjectID")
+				inter2 := strings.Trim(inter1, "  \" ")
+				inter3 := strings.Trim(inter2, "  \" ")
+				inter4 := strings.Trim(inter3, "  \"\" ")
+				inter5 := strings.Trim(inter4, "  ( ) ")
+				row := []string{inter5, baseString2, now.Format(YYYYMMDD), time.Now().Format(HHMMSS)}
+				_ = csvwriter.Write(row)
+				csvwriter.Flush()
+
+			}
+
+		}
+
+		onErrorFunc := func(err error) {
+			fmt.Errorf("failed to watch | disconnected from watch. %v", err)
+		}
+
+		onCloseFunc := func() {
+			fmt.Println("watch connection is being closed")
+			csvwriter.Flush()
+			csvfile.Close()
+		}
+
+		cli := fyerswatch.NewNotifier(apiKey, accessToken).
+			WithOnConnectFunc(onConnectFunc).
+			WithOnMessageFunc(onMessageFunc).
+			WithOnErrorFunc(onErrorFunc).
+			WithOnCloseFunc(onCloseFunc)
+
+			// cli.Subscribe(api.SymbolDataTick, "NSE:SBIN-EQ", "NSE:ONGC-EQ")
+
+		symbols := []string{"NSE:SBIN-EQ", "NSE:ONGC-EQ"}
+		// create the capped collections
+		for i := 0; i < len(symbols); i++ {
+			baseString1 := strings.ReplaceAll(symbols[i], ":", "-")
+			baseString2 := strings.ReplaceAll(baseString1, ".", "")
+			maxSize := int64(30 * 1024 * 1024)
+			client.Database(baseString2).CreateCollection(context.TODO(), now.Format(YYYYMMDD), options.CreateCollection().SetCapped(true), options.CreateCollection().SetSizeInBytes(maxSize), options.CreateCollection().SetMaxDocuments(50000))
+
+		}
+		cli.Subscribe(api.SymbolDataTick, symbols...)
 	}
-
-	onErrorFunc := func(err error) {
-		fmt.Errorf("failed to watch | disconnected from watch. %v", err)
-	}
-
-	onCloseFunc := func() {
-		fmt.Println("watch connection is closed")
-	}
-
-	cli := fyerswatch.NewNotifier(apiKey, accessToken).
-		WithOnConnectFunc(onConnectFunc).
-		WithOnMessageFunc(onMessageFunc).
-		WithOnErrorFunc(onErrorFunc).
-		WithOnCloseFunc(onCloseFunc)
-
-		// cli.Subscribe(api.SymbolDataTick, "NSE:SBIN-EQ", "NSE:ONGC-EQ")
-
-	symbols := []string{"NSE:SBIN-EQ", "NSE:ONGC-EQ"}
-	cli.Subscribe(api.SymbolDataTick, symbols...)
 
 }
