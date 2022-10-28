9a56ed11b0a1cbec321328fe21474f247e49bd59
Aloy Aditya Sen <aloysen951@gmail.com>
2022-10-29 00:43:26 +0530

    adding appropriate cpier to make the code run from one entrypoint

--- runner.bat
+++ runner.bat
@@ -1,4 +1,6 @@
 @echo off
 FOR /F %%i IN ('python.exe "./Fyers-API-Access-Token-Generation-V2-main/New_Fyers_Access_Token.py"') DO set VARIABLE=%%i
 echo %VARIABLE%
+start "CopierService" cmd /k python pyDatasetCopier.py
 go run websocket.go %VARIABLE%
+taskkill /FI "WindowTitle eq CopierService*" /T /F
